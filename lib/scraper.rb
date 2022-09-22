a225cdab656bcd0ca693d549addbd1366fe91dc1
Casey Tansey <ctansey12600@outlook.com>
2022-09-22 16:12:02 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+courses = doc.css(".heading-20-semibold.color-black.card-title-tl")
+
+courses.each do |course|
+  puts course.text.strip
+end 
